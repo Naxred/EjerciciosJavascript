1398c402f57f3fe2bce2f10304587f5b9c4e1094
Naxred <rexkid2001@gmail.com>
2022-10-19 08:44:11 -0500

    Segundo ejrecicio

--- Javascript/js5.js
+++ Javascript/js5.js
@@ -22,7 +22,34 @@ function calcular(){
 
     let input_resultado = document.getElementById("resultado");
     input_resultado.value = resultado;
+}
 
+function verImagen(){
+    let numero_imagen = document.getElementById("num_img").value;
+    console.log(numero_imagen);
 
+    let imagen_resultado = document.querySelector(".imagen_seleccionada");
+    
+    if (numero_imagen == "Alvaro"){
+        imagen_resultado.innerHTML = '<img src="./img/alvaro.jpg" alt="Imagen1">';
+    } else if (numero_imagen == "Gustavo"){
+        imagen_resultado.innerHTML = '<img src="./img/gustavo.jpg" alt="Imagen2">';
+    } else if (numero_imagen == "Roberto"){
+        imagen_resultado.innerHTML = '<img src="./img/roberto.jpg" alt="Imagen2">';
+    } else if (numero_imagen == "Santiago"){
+        imagen_resultado.innerHTML = '<img src="./img/santiago.jpg" alt="Imagen2">';
+    } else if (numero_imagen == "Santiago Travela"){
+        imagen_resultado.innerHTML = '<img src="./img/santiagotravela.jpg" alt="Imagen2">';
+    }
+
+    
+}
+
+function calcularTabla(){
+    let numero_tabla = parseInt(document.getElementById("tabla").value);
+
+    for(let i = 1; i <= 10; i++){
+        
+    }
 }
 
