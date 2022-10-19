78d9cb2108b79e116917a54f68b0d164fdfb6f8f
Naxred <rexkid2001@gmail.com>
2022-10-19 07:33:40 -0500

    Clase miercoles 19

---
+++ Javascript/calculadora.js
@@ -0,0 +1,7 @@
+function calcular(){
+    let primer_numero = parseInt(document.getElementById("primer_numero").value);
+    let segundo_numero = parseInt(document.getElementById("segundo_numero").value);
+    let operacion = document.getElementById("primer_numero").value;
+
+    console.log(primer_numero, segundo_numero, operacion);
+}
---
+++ Javascript/js4.js
@@ -0,0 +1,47 @@
+// funciones 
+
+function divisores(numero){
+    let total = 0;
+    for (let i = 0; i<=numero; i++){
+        if(numero % i == 0){
+            total++;
+        }
+    }
+    return total;
+}
+
+// funcion anonima 
+
+const divisoresV2 = function(numero){
+    let total = 0;
+    for (let i = 0; i<=numero; i++){
+        if(numero % i == 0){
+            total++;
+        }
+    }
+    return total;
+};
+
+const divisoresV3 = numero => {
+    let total = 0;
+    for (let i = 0; i<=numero; i++){
+        if(numero % i == 0){
+            total++;
+        }
+    }
+    return total;
+};
+
+function calcular(){
+    let numero = parseInt(document.getElementById("num").value);
+
+    let resultado = document.getElementById("resul");
+    resultado.value = divisores(numero);
+
+}
+
+
+// Llamar a la funcion
+console.log("divisores", divisores(25));
+console.log(divisoresV2(20));
+console.log(divisoresV3(30));
