846497cc8d9668c9875227fbecd408a046bbb11e
Naxred <rexkid2001@gmail.com>
2022-10-19 09:21:16 -0500

    tercer ejercicio

--- Javascript/js5.js
+++ Javascript/js5.js
@@ -47,9 +47,17 @@ function verImagen(){
 
 function calcularTabla(){
     let numero_tabla = parseInt(document.getElementById("tabla").value);
-
+    let resultado_tabla = document.querySelector("#resultado_tabla");
+    let info = "";
+    let arreglo = []
     for(let i = 1; i <= 10; i++){
-        
+        let resultado = i * numero_tabla;
+        console.log(i, " x ", numero_tabla, " = ", resultado);
+        info = "<br>" + i + "x" + numero_tabla + "=" + resultado
+        arreglo.push(info);
     }
+    resultado_tabla.innerHTML = arreglo;
 }
+
+
 
